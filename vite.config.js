2f70fdf002e6efa03e3cc42117b86910eae15d68
abd33 <alaziizz67@gmail.com>
2026-05-19 00:54:01 +0300

    Fixed the Vercel blank screen cause. fixed the favicon base URL double-slash issue.

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mika-creation-studio/',
-  plugins: [
-    react(),
-    viteCompression({ algorithm: "gzip" }),
-    viteCompression({ algorithm: "brotliCompress" }),
-  ],
+  base: '/',
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          "react-vendor": ["react", "react-dom"],
-          "gsap-vendor": ["gsap", "@gsap/react"],
+          "react": ["react", "react-dom"],
+          "gsap": ["gsap"],
         },
       },
     },
